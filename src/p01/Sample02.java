5744c73014d9c49b7735acbb136e1884f2492b53
762-student034 <762-student034@la-study.com>
2026-04-15 14:59:40 +0900

    1st

--- src/p01/Sample02.java
+++ src/p01/Sample02.java
@@ -3,7 +3,11 @@ package p01;
 public class Sample02 {
 
 	public static void main(String[] args) {
-		System.out.println("エルトン");
-		System.out.println(29);
+		String name = "エルトン";	
+		int age = 29;
+		
+		
+		System.out.println(name);
+		System.out.println(age);
 	}
 }
