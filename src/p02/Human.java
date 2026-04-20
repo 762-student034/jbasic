7f2b6a5b767043d41e40d92f9aa2f010a0be79db
762-student034 <762-student034@la-study.com>
2026-04-20 13:05:15 +0900

    first commit

---
+++ src/p02/Human.java
@@ -0,0 +1,47 @@
+package p02;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human() {
+        this.name = "Unknown";
+        this.age = 0;
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // --- Proper Getters ---
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // --- Proper Setters ---
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void introduce() {
+        System.out.println("名前は" + this.name);
+        System.out.println("年齢は" + this.age);
+    }
+
+    public void eat() {
+        System.out.println("食事中です。");
+    }
+
+    public void talk() {
+        System.out.println("会話中です。");
+    }
+}
---
+++ src/p02/Start.java
@@ -0,0 +1,14 @@
+package p02;
+
+
+public class Start {
+
+	public static void main(String[] args) {
+		
+		Human tanaka = new Human("田中太郎", 20);
+
+		tanaka.introduce();
+		
+	}
+
+}
