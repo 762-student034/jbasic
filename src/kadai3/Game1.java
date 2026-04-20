a8639a80fe0bfbccef3a9e0dfba4a6fe3613e391
762-student034 <762-student034@la-study.com>
2026-04-20 10:07:19 +0900

    > game 1 gameLogic reworked to recursive because I can

--- src/kadai3/Game1.java
+++ src/kadai3/Game1.java
@@ -7,35 +7,29 @@ public class Game1 {
 
 	public static void main(String[] args) {
 		Random rand = new Random();
-		
+
 		int answer = rand.nextInt(100);
-		int times = 0;
-		
-		System.out.println("デバッグ用: " + answer);
+
+//		System.out.println("デバッグ用: " + answer);
 		Scanner scan = new Scanner(System.in);
-		while (true) {
-			System.out.println("数字を入力してください：");
-			int input = scan.nextInt();
-			if (gameLogic(answer, input) == true) {
-				times++;
-			} else {
-				break;
-			}
-		}
+		int times = gameLogic(answer, scan, 0); 
 
 		System.out.println(times + "回で当たりました！");
 		scan.close();
 	}
 
-	public static boolean gameLogic(int answer, int input) {
-
+	public static int gameLogic(int answer, Scanner scan, int times) {
+		System.out.println("**数字を入力してください: ");
+        int input = scan.nextInt();
+        times++;
 		if (input > answer) {
 			System.out.println("**大きいすぎです。");
-			return true;
+			return gameLogic(answer, scan, times);
 		} else if (input < answer) {
 			System.out.println("**小さすぎです。");
-			return true;
+			return gameLogic(answer, scan, times);
 		}
-		return false;
+		System.out.println("正解です！");
+		return times;
 	}
 }
